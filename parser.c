ba1b0c1b9bb11b3ec803e6e529cf17c66a46ce7a
aouelete <aouellette2016@gmail.com>
2015-07-23 14:53:59 -0400

    fixed indentation

--- parser.c
+++ parser.c
@@ -6,87 +6,86 @@
 #include "parser.h"
 
 char* get_vals(char* str) {
-  int start = 0;
-  int end = 0;
-  int index = 0;
-  while(str[index] != '\0') {
-    if(str[index] == '\"') {
-      start = end;
-      end = index + 1;
+    int start = 0;
+    int end = 0;
+    int index = 0;
+    while(str[index] != '\0') {
+        if(str[index] == '\"') {
+            start = end;
+            end = index + 1;
+        }
+        index++;
     }
-    index++;
-  }
-  end--;
-  char* out =(char*) malloc(sizeof(char)*(end - start + 1));
-  strncpy(out , &str[start], end - start);
-  out[end - start] = '\0';
-  return out;
+    end--;
+    char* out =(char*) malloc(sizeof(char)*(end - start + 1));
+    strncpy(out , &str[start], end - start);
+    out[end - start] = '\0';
+    return out;
 }
 
 void sort_games(game_t** games, int size) {
-  for(int i = 1; i < size; i++) {
-    for(int j = 1; j < size; j++) {
-      if(strcmp(games[j - 1]->name, games[j]->name) > 0) {
-        game_t* tmp = games[j];
-        games[j] = games[j - 1];
-        games[j - 1] = tmp;
-      }
+    for(int i = 1; i < size; i++) {
+        for(int j = 1; j < size; j++) {
+            if(strcmp(games[j - 1]->name, games[j]->name) > 0) {
+                game_t* tmp = games[j];
+                games[j] = games[j - 1];
+                games[j - 1] = tmp;
+            }
+        }
     }
-  }
 }
 
 void parse_manifests(int* size, int* capacity, game_t*** games, char* steam_path, int is_wine) {
-  // Set up directory streams and whatnot
-  DIR* d;
-  struct dirent* dir;
-  FILE* f;
-  char buf[256];
-  d = opendir(steam_path);
+    // Set up directory streams and whatnot
+    DIR* d;
+    struct dirent* dir;
+    FILE* f;
+    char buf[256];
+    d = opendir(steam_path);
 
-  // Get all files in the steam path dir
-  if(d) {
-    while((dir = readdir(d)) != NULL) {
-      if(strstr(dir->d_name, "appmanifest") != NULL) {
+    // Get all files in the steam path dir
+    if(d) {
+        while((dir = readdir(d)) != NULL) {
+            if(strstr(dir->d_name, "appmanifest") != NULL) {
 
-        // Create and fill out game struct
-        game_t* g = (game_t*) malloc(sizeof(game_t));
-        g->is_wine = is_wine;
+                // Create and fill out game struct
+                game_t* g = (game_t*) malloc(sizeof(game_t));
+                g->is_wine = is_wine;
 
-        // Construct the full path to the file
-        char* full_path;
-        asprintf(&full_path, "%s%s", steam_path, dir->d_name);
-        f = fopen(full_path, "r");
-        if(f == NULL) {
-          perror("Error opening steam path:");
-          exit(1);
+                // Construct the full path to the file
+                char* full_path;
+                asprintf(&full_path, "%s%s", steam_path, dir->d_name);
+                f = fopen(full_path, "r");
+                if(f == NULL) {
+                    perror("Error opening steam path:");
+                    exit(1);
+                }
+
+                while(fgets(buf, sizeof(buf), f)) {
+                    if(strstr(buf, "appid") != NULL) {
+                        g->appid = get_vals(buf);
+                    } else if(strstr(buf, "name") != NULL) {
+                        g->name = get_vals(buf);
+                    }
+                }
+                fclose(f);
+                (*games)[*size] = g;
+                *size += 1;
+
+                // Free the memory used by the string
+                free(full_path);
+                full_path = NULL;
+
+                //Resize array as needed
+                if(*size >= *capacity) {
+                    *games = (game_t**) realloc(*games, *capacity*2*sizeof(game_t*));
+                    *capacity *= 2;
+                }
+            }
         }
-
-        while(fgets(buf, sizeof(buf), f)) {
-          if(strstr(buf, "appid") != NULL) {
-            g->appid = get_vals(buf);
-          } else if(strstr(buf, "name") != NULL) {
-            g->name = get_vals(buf);
-          }
-        }
-        fclose(f);
-        (*games)[*size] = g;
-        *size += 1;
-
-        // Free the memory used by the string
-        free(full_path);
-        full_path = NULL;
-
-        //Resize array as needed
-        if(*size >= *capacity) {
-          *games = (game_t**) realloc(*games, *capacity*2*sizeof(game_t*));
-          *capacity *= 2;
-        }
-      }
+        closedir(d);
+    } else {
+        printf("Could not open steam directory %s\n", steam_path);
     }
-    closedir(d);
-  } else {
-    printf("Could not open steam directory %s\n", steam_path);
-  }
 }
-
 
